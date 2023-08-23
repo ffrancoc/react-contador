522471ac4d67e47b5256b556b935465373c6a827
Francisco Curín <jose2fcurin@gmail.com>
2023-08-22 19:44:06 -0600

    Actualización de código de la aplicación

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import './App.css';
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import './App.css'
 
-function App() {
+function App({value}) {
+
+  const [contador, setContador] = useState(value)
+
+  const incrementHandle = () => {
+    setContador(contador + 1)
+  }
+
+  const decrementHandle = () => {
+    if ( contador > 0 ){
+      setContador(contador - 1)
+    }
+  }
+
+  const resetHandle = () => {
+    setContador(value)
+  }
+
   return (
     <div className='app-container'>
       <h1>Simple contador en React</h1>
-      <h2>0</h2>
+      <h2>{ contador }</h2>
       <div>
-        <button>+</button>
-        <button>-</button>
-        <button>Reset</button>
+        <button onClick={ incrementHandle }>+</button>
+        <button onClick={ decrementHandle }>-</button>
+        <button onClick={ resetHandle }>Reset</button>
       </div>
     </div>
   )
+}
+
+App.propTypes = {
+  value: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  value: 10
 }
 
 export default App
